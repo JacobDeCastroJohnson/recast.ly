6be469eb803c91496a0a5de7b4568c9532537d7f
JacobDeCastroJohnson <Jacob.DeCastro.J@gmail.com>
2020-09-01 20:04:55 -0700

    Implement VideoList, VideoListEntry, VideoPlayer and App functionality for example API Data

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,20 +1,27 @@
-import exampleVideoData from '../data/exampleVideoData.js';
+// import exampleVideoData from '../data/exampleVideoData.js';
+import VideoListEntry from './VideoListEntry.js';
 
-var VideoList = () => (
-  <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-  </div>
-);
+var VideoList = (props) => {
+  return (<div className="video-list">
+    {props.videos.map((video, i) => {
+      return (<VideoListEntry
+        currentVideo={props.currentVideo}
+        handleVideoPlayerClick={props.handleVideoPlayerClick}
+        video={video}
+        key={i} />
+      );
+    }
+    )}
+  </div>);
+
+};
+
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
-VideoList.propTypes = {
-  videos: PropTypes.array.isRequired
-};
+// VideoList.propTypes = {
+//   // videos: PropTypes.isRequired
+// };
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
 // `var` declarations will only exist globally where explicitly defined.
